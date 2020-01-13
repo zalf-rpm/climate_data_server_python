7f862e63f2f01e83975ac0abccfe96aca9af4f7f
Michael Berg <michael.berg@zalf.de>
2020-01-13 15:09:37 +0100

    Update cluster_runtime.py

--- cluster_runtime.py
+++ cluster_runtime.py
@@ -34,49 +34,67 @@ class SlurmRuntime(cluster_admin_service_capnp.Cluster.Runtime.Server):
     def delFactory(self, aModelId):
         # remove factor for given model id
         del self._factories[aModelId]
-        
-        # unregister factory at admin_master 
-        del self._unregs[aModelId] # works because deleting object will just do the same as unregistering
-        #self._unregs.pop(aModelId).unregister()
 
-    def registerModelInstanceFactory(self, aModelId, aFactory, _context, **kwargs): # registerModelInstanceFactory @0 (aModelId :Text, aFactory :ModelInstanceFactory) -> (unreg :Common.Unregister);
+        # unregister factory at admin_master
+        # works because deleting object will just do the same as unregistering
+        del self._unregs[aModelId]
+        # self._unregs.pop(aModelId).unregister()
+
+    # registerModelInstanceFactory @0 (aModelId :Text, aFactory :ModelInstanceFactory) -> (unreg :Common.Unregister);
+    def registerModelInstanceFactory(self, aModelId, aFactory, _context, **kwargs):
         "register a model instance factory for the given model id"
         self._factories[aModelId] = aFactory
         self._unregs[aModelId] = self._admin_master.registerModelInstanceFactory(aModelId, aFactory).unregister
         return common.CallbackImpl(self.delFactory, aModelId, exec_callback_on_del=True)
 
-    def availableModels_context(self, context): # availableModels @1 () -> (factories :List(ModelInstanceFactory));
+    # availableModels @1 () -> (factories :List(ModelInstanceFactory));
+    def availableModels_context(self, context):
         "# the model instance factories this runtime has access to"
         pass
 
-    def numberOfCores_context(self, context): # numberOfCores @2 () -> (cores :Int16);
+    # numberOfCores @2 () -> (cores :Int16);
+    def numberOfCores_context(self, context):
         "# how many cores does the runtime offer"
-        return self._cores 
+        return self._cores
 
-    def freeNumberOfCores_context(self, context): # freeNumberOfCores @3 () -> (cores :Int16);
+    # freeNumberOfCores @3 () -> (cores :Int16);
+    def freeNumberOfCores_context(self, context):
         "# how many cores are still unused"
         return self._cores - self._used_cores
 
-    def reserveNumberOfCores_context(self, context): # reserveNumberOfCores @4 (reserveCores :Int16, aModelId :Text) -> (reservedCores :Int16);
+    # reserveNumberOfCores @4 (reserveCores :Int16, aModelId :Text) -> (reservedCores :Int16);
+    def reserveNumberOfCores_context(self, context):
         "# try to reserve number of reserveCores for model aModelId and return the number of actually reservedCores"
         pass
 
+
 def main():
+    config = {
+        "admin_master_address": "localhost:8000"
+    }
+    # read commandline args only if script is invoked directly from commandline
+    if len(sys.argv) > 1 and __name__ == "__main__":
+        for arg in sys.argv[1:]:
+            k, v = arg.split("=")
+            if k in config:
+                config[k] = v
+
+    print("config:", config)
+
     #address = parse_args().address
 
     master_available = False
     while not master_available:
         try:
-            admin_master = capnp.TwoPartyClient("localhost:8000").bootstrap().cast_as(cluster_admin_service_capnp.Cluster.AdminMaster)
+            admin_master = capnp.TwoPartyClient(config["admin_master_address"]).bootstrap().cast_as(cluster_admin_service_capnp.Cluster.AdminMaster)
             master_available = True
         except:
-            #time.sleep(1)
+            # time.sleep(1)
             pass
 
-    
     #runtime = SlurmRuntime(cores=4, admin_master=admin_master)
     #registered_ = False
-    #while not registered_factory:
+    # while not registered_factory:
     #    try:
     #        runtime.registerModelInstanceFactory("monica_v2.1", monicaFactory).wait()
     #        registered_factory = True
@@ -84,10 +102,10 @@ def main():
     #        time.sleep(1)
     #        pass
 
-
     #server = capnp.TwoPartyServer("*:8000", bootstrap=DataServiceImpl("/home/berg/archive/data/"))
     server = capnp.TwoPartyServer("*:9000", bootstrap=SlurmRuntime(cores=4, admin_master=admin_master))
     server.run_forever()
+
 
 if __name__ == '__main__':
     main()
